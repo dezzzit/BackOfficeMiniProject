1d4a1679ff15b438c64b3b86c96716540f2e0381
stanislav_lion <zrozhevsky@gmail.com>
2020-04-11 14:37:23 +0300

    Move resources to main project. Refactoting ModelBuilderExtensions.

--- BackOfficeMiniProject.DataAccess.Database/ModelBuilderExtensions.cs
+++ BackOfficeMiniProject.DataAccess.Database/ModelBuilderExtensions.cs
@@ -1,36 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using BackOfficeMiniProject.DataAccess.Database.DataFileParsers;
 using BackOfficeMiniProject.DataAccess.DataModels;
 using Microsoft.EntityFrameworkCore;
 
 namespace BackOfficeMiniProject.DataAccess.Database
 {
-    static class ModelBuilderExtensions
+    public static class ModelBuilderExtensions
     {
+        public static string Resources = nameof(Resources);
+
+        private static readonly string _brandFileName = "Brands.tsv";
+        private static readonly string _orderFileName = "Brand_Quantity_Time_Received.tsv";
+
         public static void Seed(this ModelBuilder modelBuilder)
         {
-            const string brandFileName = "Brands.tsv";
-            const string orderFileName = "Brand_Quantity_Time_Received.tsv";
+            string resourceDirPath = Path.Combine(Directory.GetCurrentDirectory(), Resources);
 
-            string resourceDirPath =
-                Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase).Substring(6) + "/Resources/";
-            string brandFilePath = resourceDirPath + brandFileName;
-            string orderFilePath = resourceDirPath + orderFileName;
+            string brandFilePath = Path.Combine(resourceDirPath, _brandFileName);
+            string orderFilePath = Path.Combine(resourceDirPath, _orderFileName);
 
             var brandPropertyToHeaderMap = new Dictionary<string, string> { { nameof(Brand.Id), "BRAND_ID" }, { nameof(Brand.Name), "Name" }};
             var brandParser = new BrandParser(brandFilePath,brandPropertyToHeaderMap);
-            var brands = brandParser.GetBrands();
+            
+            List<Brand> brands = brandParser.GetBrands();
+            
             modelBuilder.Entity<Brand>().HasData(
                 brands
             );
 
             var orderPropertyToHeaderMap = new Dictionary<string, string> { { nameof(Order.TimeReceived), "TIME_RECEIVED" }, { nameof(Order.Quantity), "QUANTITY" }, { nameof(Order.BrandId), "BRAND_ID" } };
             var orderParser = new OrderParser(orderFilePath, orderPropertyToHeaderMap);
-            var orders = orderParser.GetOrders();
+            
+            List<Order> orders = orderParser.GetOrders();
 
             modelBuilder.Entity<Order>().HasData(
                 orders
