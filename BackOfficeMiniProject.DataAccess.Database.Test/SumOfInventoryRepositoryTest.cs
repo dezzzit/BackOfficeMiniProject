a00b0f50ebed717de3e9224d8a6e7a34688c5b2c
stanislav_lion <zrozhevsky@gmail.com>
2020-04-12 21:39:15 +0300

    Added SumOfInventoryRepositoryTest. Fixed BrandRepositoryTests.

---
+++ BackOfficeMiniProject.DataAccess.Database.Test/SumOfInventoryRepositoryTest.cs
@@ -0,0 +1,76 @@
+﻿using BackOfficeMiniProject.DataAccess.Database.Context;
+using BackOfficeMiniProject.DataAccess.Database.Repositories;
+using BackOfficeMiniProject.DataAccess.DataModels;
+using BackOfficeMiniProject.DataAccess.Repository;
+using BackOfficeMiniProject.Reports.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace BackOfficeMiniProject.DataAccess.Database.Test
+{
+    public class SumOfInventoryRepositoryTest : IDisposable
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly ISumOfInventoryRepository _sumOfInventoryRepository;
+
+        protected DbContextOptions<BackOfficeDbContext> dbContextOptions { get; }
+        protected BackOfficeDbContext context;
+        protected string connectionString = "Server=localhost;port=3306;Database=Test_SumOfInventory;User=root;Password=12345;";
+
+        public SumOfInventoryRepositoryTest()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<BackOfficeDbContext>()
+                .UseMySql(connectionString)
+                .Options;
+
+            context = new BackOfficeDbContext(dbContextOptions);
+
+            context.Database.EnsureCreated();
+
+            _brandRepository = new BrandRepository(context);
+            _sumOfInventoryRepository = new SumOfInventoryRepository(context);
+        }
+        
+        public void Dispose()
+        {
+            context.Database.EnsureDeleted();
+        }
+
+        [Fact]
+        public void Test_SumOfInventory()
+        {
+            int expectedBrandCount = context.Brands
+                .Count();
+
+            int[] expectedBrandIds = context.Brands
+                .Select(b => b.Id)
+                .ToArray();
+
+            List<SumOfInventory> actualSumOfInventories = _sumOfInventoryRepository.SumOfInventory
+                .ToList();
+
+            foreach (var expectedBrandId in expectedBrandIds)
+            {
+                Brand expectedBrand = _brandRepository.Get(expectedBrandId);
+                Assert.NotNull(expectedBrand);
+
+                int expectedSum = context.Orders
+                    .Where(o => o.BrandId == expectedBrand.Id)
+                    .Select(s => s.Quantity)
+                    .Sum();
+
+                SumOfInventory actualSumOfInventory = actualSumOfInventories
+                    .Where(i => i.BrandName.Equals(expectedBrand.Name))
+                    .FirstOrDefault();
+
+                Assert.Equal(expectedBrand.Name, actualSumOfInventory.BrandName);
+                Assert.Equal(expectedSum, actualSumOfInventory.Quantity);
+            }
+
+            Assert.Equal(expectedBrandCount, actualSumOfInventories.Count);
+        }
+    }
+}
